8249121e94e856d4dbcf6724d53dd124984b4e08
Марина Агалакова <agalakova.m.a@gmail.com>
2020-07-23 15:30:39 +0300

    add conditions for components render

--- src/App.tsx
+++ src/App.tsx
@@ -47,22 +47,32 @@ const App = () => {
   return (
     <div className="App">
       <h1>React Quiz</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">Score:</p>
-      <p>Loading questions...</p>
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next question
-      </button>
+      {(gameOver || userAnswers.length === TOTAL_QUESTIONS) && (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      )}
+
+      {!gameOver && <p className="score">Score:</p>}
+      {loading && <p>Loading questions...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!loading &&
+        !gameOver &&
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 && (
+          <button className="next" onClick={nextQuestion}>
+            Next question
+          </button>
+        )}
     </div>
   );
 };
